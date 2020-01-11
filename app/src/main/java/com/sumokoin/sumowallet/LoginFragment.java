4d298adf65e7a7fffc248a40b60195343a95dd2f
Sumo Gr <sumogr@mail.com>
2020-01-11 11:38:00 +0200

    Update LoginFragment.java

--- app/src/main/java/com/sumokoin/sumowallet/LoginFragment.java
+++ app/src/main/java/com/sumokoin/sumowallet/LoginFragment.java
@@ -47,55 +47,6 @@ import com.sumokoin.sumowallet.util.KeyStoreHelper;
 import com.sumokoin.sumowallet.util.Notice;
 import com.sumokoin.sumowallet.widget.Toolbar;
 
-/*
- * Copyright (c) 2017 m2049r
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.sumokoin.sumowallet;
-
-import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.sumokoin.sumowallet.data.NodeInfo;
-import com.sumokoin.sumowallet.layout.NodeInfoAdapter;
-import com.sumokoin.sumowallet.layout.WalletInfoAdapter;
-import com.sumokoin.sumowallet.model.WalletManager;
-import com.sumokoin.sumowallet.util.Helper;
-import com.sumokoin.sumowallet.util.KeyStoreHelper;
-import com.sumokoin.sumowallet.util.Notice;
-import com.sumokoin.sumowallet.widget.Toolbar;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
